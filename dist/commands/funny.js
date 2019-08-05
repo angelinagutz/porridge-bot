ae3009cd621037ea2bcbd628c0623284d4357820
angelinagutz <angelina.ogutierrez@gmail.com>
2019-08-05 14:00:58 -0400

    Updated p!funny; user can now get a specific image

--- dist/commands/funny.js
+++ dist/commands/funny.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Discord = require("discord.js");
+var maxNum = 128;
 class funny {
     constructor() {
         this._command = "funny";
@@ -12,7 +13,19 @@ class funny {
         return command === this._command;
     }
     runCommand(args, msgObject, bot) {
-        var imageNum = Math.floor(Math.random() * (120 - 1 + 1) + 1);
+        var getFileName = msgObject.content.split(" ");
+        if (!getFileName[1]) {
+            var imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+        }
+        else {
+            if (Number(getFileName[1]) > maxNum) {
+                msgObject.channel.send("```Sorry, there's no image with that number. Have a random image instead!```");
+                imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+            }
+            else {
+                imageNum = Number(getFileName[1]);
+            }
+        }
         var attachment = new Discord.Attachment(`https://raw.githubusercontent.com/angelinagutz/porridge-bot/master/assets/funny/${imageNum}.PNG`);
         msgObject.channel.send(attachment);
     }
--- src/commands/funny.ts
+++ src/commands/funny.ts
@@ -2,7 +2,10 @@
 import * as Discord from "discord.js";
 import {IBotCommand} from "../api";
 
+var maxNum = 128;
+
 export default class funny implements IBotCommand {
+
 
     private readonly _command = "funny";
 
@@ -15,7 +18,28 @@ export default class funny implements IBotCommand {
 
     }
     runCommand(args: string[], msgObject: Discord.Message, bot: Discord.Client): void {
-        var imageNum = Math.floor(Math.random() * (120 - 1 + 1) + 1);
+        
+        var getFileName = msgObject.content.split(" ");
+
+        if (!getFileName[1]) {
+        
+        var imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+
+        }
+
+        else {
+            if (Number(getFileName[1]) > maxNum) {
+
+                msgObject.channel.send("```Sorry, there's no image with that number. Have a random image instead!```");
+                imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+            
+            }
+
+            else {
+
+                imageNum = Number(getFileName[1]);
+            }
+        }
 
         var attachment = new Discord.Attachment(`https://raw.githubusercontent.com/angelinagutz/porridge-bot/master/assets/funny/${imageNum}.PNG`);
         msgObject.channel.send(attachment);
