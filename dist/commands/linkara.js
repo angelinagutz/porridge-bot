b545d9580603618e57b53161e87b3111d171c51a
angelinagutz <angelina.ogutierrez@gmail.com>
2019-09-15 20:46:07 -0400

    Added functionality and assets for p!linkara

--- dist/commands/linkara.js
+++ dist/commands/linkara.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Discord = require("discord.js");
+var maxNum = 40;
 class linkara {
     constructor() {
         this._command = "linkara";
@@ -11,6 +13,21 @@ class linkara {
         return command === this._command;
     }
     runCommand(args, msgObject, bot) {
+        var getFileName = msgObject.content.split(" ");
+        if (!getFileName[1]) {
+            var imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+        }
+        else {
+            if (Number(getFileName[1]) > maxNum) {
+                msgObject.channel.send("```Sorry, there's no image with that number. Have a random image instead!```");
+                imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+            }
+            else {
+                imageNum = Number(getFileName[1]);
+            }
+        }
+        var attachment = new Discord.Attachment(`https://raw.githubusercontent.com/angelinagutz/porridge-bot/master/assets/linkara/${imageNum}.png`);
+        msgObject.channel.send(attachment);
     }
 }
 exports.default = linkara;
--- src/commands/linkara.ts
+++ src/commands/linkara.ts
@@ -1,6 +1,8 @@
 import * as Discord from "discord.js";
 import {IBotCommand} from "../api";
 
+var maxNum = 40;
+
 export default class linkara implements IBotCommand {
 
     private readonly _command = "linkara";
@@ -15,7 +17,32 @@ export default class linkara implements IBotCommand {
     }
     runCommand(args: string[], msgObject: Discord.Message, bot: Discord.Client): void {
 
-       
+        var getFileName = msgObject.content.split(" ");
+
+        if (!getFileName[1]) {
+        
+        var imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+
+        }
+
+        else {
+            if (Number(getFileName[1]) > maxNum) {
+
+                msgObject.channel.send("```Sorry, there's no image with that number. Have a random image instead!```");
+                imageNum = Math.floor(Math.random() * (maxNum - 1 + 1) + 1);
+            
+            }
+
+            else {
+
+                imageNum = Number(getFileName[1]);
+            }
+        }
+
+        var attachment = new Discord.Attachment(`https://raw.githubusercontent.com/angelinagutz/porridge-bot/master/assets/linkara/${imageNum}.png`);
+        msgObject.channel.send(attachment);
+
+    }
         
     }
 }
